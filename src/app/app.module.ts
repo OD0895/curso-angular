80801fb0f23b527e0cbaf71dfd9887842e384dbc
Osvaldo Amador Salinas <osvaldodaniel0895@gmail.com>
2021-04-08 22:55:55 -0500

    :lipstick: [ADD] Module contact and install Angular Material

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -13,6 +13,7 @@ import { LayoutComponent } from './layout/layout.component';
 
 import { SharedModule } from './shared/shared.module';
 import { CoreModule } from './core/core.module';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 @NgModule({
   declarations: [
     AppComponent,
@@ -27,7 +28,8 @@ import { CoreModule } from './core/core.module';
     FormsModule,
     SharedModule,
     CoreModule,
-    SwiperModule
+    SwiperModule,
+    BrowserAnimationsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
--- src/app/contact/contact.module.ts
+++ src/app/contact/contact.module.ts
@@ -3,10 +3,12 @@ import { CommonModule } from '@angular/common';
 
 import { ContactComponent } from './components/contact/contact.component'
 import { ContactRoutingModule } from './contact-routing.module';
+import { FormComponent } from './components/form/form.component';
 
 @NgModule({
   declarations: [
-    ContactComponent
+    ContactComponent,
+    FormComponent
   ],
   imports: [
     CommonModule,
